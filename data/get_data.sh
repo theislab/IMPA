12039f6a40fba68acc6b737df1e8d5fe9ce191b7
ICB Alessandro Palma <alessandro.palma@vicb-submit-01.scidom.de>
2022-03-03 09:18:02 +0100

    Remove chaced files

--- data/get_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /storage/groups/ml01/workspace/alessandro.palma/cellpainting
-for dataset in dataset06.tar dataset07.tar
-do
-echo Downloading dataset $dataset 
-wget https://ml.jku.at/software/cellpainting/dataset/$dataset
-done 
