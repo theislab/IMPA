b9efa0fe5e6b8c5d0133e4f2190e5d804f4ecb76
ICB Alessandro Palma <alessandro.palma@hpc-submit01.scidom.de>
2022-05-11 16:44:03 +0200

    Fixes on the resnet drit network, addition of common classifier/discriminator

--- compert/model/modules/adversarial/adversarial_nets.py
+++ compert/model/modules/adversarial/adversarial_nets.py
@@ -1,3 +1,4 @@
+from tkinter import S
 import numpy as np
 
 import torch
@@ -13,13 +14,13 @@ The latent discriminator network
 """
 
 class LeakyReLUConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding=0, norm='None'):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding=0, norm='Instance'):
         super(LeakyReLUConv2d, self).__init__()
         model = []
         model += [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=True)]
         # Normalization layer, can be instance or batch
         if norm == 'Instance':
-            model += [nn.InstanceNorm2d(out_channels, affine=False)]
+            model += [nn.InstanceNorm2d(out_channels, affine=True)]
         if norm == 'Batch':
             model += [nn.BatchNorm2d(out_channels)]
         # Leaky ReLU loss with default parameters
@@ -34,12 +35,13 @@ The latent discriminator: tries to predict drugs on the latent space
 """
 
 class DiscriminatorNet(nn.Module):
-  def __init__(self, init_fm, out_fm, depth, num_outputs):
+  def __init__(self, init_fm, out_fm, depth, num_outputs, norm='Instance'):
     super(DiscriminatorNet, self).__init__()
     self.init_fm = init_fm
     self.out_fm = out_fm 
     self.depth = depth 
     self.num_outpus = num_outputs
+    self.norm = norm
 
     # First number of feature maps 
     in_fm = self.init_fm 
@@ -47,7 +49,7 @@ class DiscriminatorNet(nn.Module):
     # Go as deep as necessary to transform the spatial dimension to 1 
     model = []
     for i in range(depth):
-        model += [LeakyReLUConv2d(in_fm, out_fm, kernel_size=3, stride=2, padding=1)]
+        model += [LeakyReLUConv2d(in_fm, out_fm, kernel_size=3, stride=2, padding=1, norm=self.norm)]
         if i == 0:
             in_fm = out_fm
 
@@ -151,7 +153,7 @@ class DisentanglementClassifier(nn.Module):
 
 
 """
-GAN discriminator for prestine vs true
+GAN discriminator for prestine vs true based on patchGAN 
 """
 
 class GANDiscriminator(nn.Module):
@@ -168,13 +170,13 @@ class GANDiscriminator(nn.Module):
 
         # Four layers of strided LeakyReLU
         model = []
-        for i in range(4):
-            model += [LeakyReLUConv2d(in_fm, out_fm, kernel_size=4, stride=2, padding=1, norm='Batch')]
+        for i in range(5):
+            model += [LeakyReLUConv2d(in_fm, out_fm, kernel_size=4, stride=2, padding=1, norm='none')]
             in_fm = out_fm
             out_fm = in_fm*2
 
         # Last convolution with stride 1
-        model += [nn.Conv2d(in_fm, 1, kernel_size=4, stride=1, padding=1)]
+        model += [nn.Conv2d(in_fm, 1, kernel_size=4, stride=1, padding=1, bias=False)]
 
         # Compile model 
         self.conv = nn.Sequential(*model)
@@ -189,23 +191,30 @@ class GANDiscriminator(nn.Module):
 
     def discriminator_pass(self, X, X_hat, loss):
         self.train()
-        data_real = X  # From batch
-        data_fake = X_hat.detach()  # From the autoencoder
+        # From batch
+        data_real = X
+        # From the autoencoder model 
+        data_fake = X_hat.detach()  
+        # Assign labels 
         label_real = torch.ones(data_real.shape[0]).to(self.device) 
         label_fake = torch.zeros(data_fake.shape[0]).to(self.device)
-
+        
+        # Predict on the real dataset vs the fake one 
         pred_real = self.forward(data_real)
         pred_fake = self.forward(data_fake)
         gan_loss_real = loss(pred_real, label_real)
         gan_loss_fake = loss(pred_fake, label_fake)
+        # Average losses 
         return (gan_loss_real + gan_loss_fake)/2
 
     def generator_pass(self, X_hat, loss):
+        # Put model in eval mode in case there are BN layers 
         self.eval()
-        labels = torch.ones(X_hat.shape[0]).to(self.device)   # Define y_hat as true 
+        labels = torch.ones(X_hat.shape[0]).to(self.device)  
         pred = self.forward(X_hat)  # Prediction by the model on the generated images (single value per batch observation)
-
-        gan_loss = loss(pred, labels)  # GAN loss function (cross entropy)
+        
+        # GAN loss function (cross entropy)
+        gan_loss = loss(pred, labels)  
         return gan_loss
 
  
@@ -228,32 +237,26 @@ class GANClassifier(nn.Module):
         out_fm = self.init_fm
 
         model = []
-        for i in range(4):
-            model += [LeakyReLUConv2d(in_fm, out_fm, kernel_size=4, stride=2, padding=1, norm='Batch')]
+        for i in range(5):
+            model += [LeakyReLUConv2d(in_fm, out_fm, kernel_size=4, stride=2, padding=1, norm='none')]
             in_fm = out_fm
             out_fm *= 2
         
         # Dimension of the latent 
-        flattened_dim = (self.init_dim//16)**2  * in_fm  # Division by 16 because we use 4 2x2 strided convolutions 
+        model += [torch.nn.Conv2d(in_fm, self.num_outputs_drug, kernel_size=3, bias=False)]  # Division by 16 because we use 4 2x2 strided convolutions 
         
         # Compile model 
         self.conv = nn.Sequential(*model)
-        
-        # Linear classification layer
-        self.linear_drug = torch.nn.Linear(flattened_dim, self.num_outputs_drug)
-
 
     def forward(self, x):
-        out = self.conv(x)
-        # Output drug
-        out_drug = self.linear_drug(out.view(out.shape[0], -1))
-        return out_drug
+        out = self.conv(x).view(x.shape[0], -1)
+        return out
     
     def discriminator_pass(self, X, loss, labels_drug):
         self.train()
         out_drug = self.forward(X)
         # Given input image X, train the classifier on the true label 
-        loss_drug = loss(out_drug, labels_drug)
+        loss_drug = loss(out_drug, labels_drug)  # Cross entropy 
         return loss_drug
 
     def generator_pass(self, X_counterf, loss, swapped_idx_drugs):
@@ -264,6 +267,78 @@ class GANClassifier(nn.Module):
         gan_loss_drug = loss(pred_drug, swapped_idx_drugs)  # GAN loss function (cross entropy)
         return gan_loss_drug
 
+"""
+Implement discriminator and classifier together 
+"""
+
+class DiscriminatorClassifier(nn.Module):
+    def __init__(self, init_dim, in_channels, init_fm, num_outputs_drug, device):
+
+        super(DiscriminatorClassifier, self).__init__()
+        self.device = device
+        self.init_dim = init_dim  # Starting spatial dimension 
+        self.in_channels = in_channels  # Spatial dimension
+        self.init_fm = init_fm  # Input feature maps
+        self.num_outputs_drug = num_outputs_drug  # Number of classes for the classification 
+
+        # First number of feature maps 
+        in_fm = self.in_channels
+        out_fm = self.init_fm
+
+        model = []
+        for i in range(5):
+            model += [LeakyReLUConv2d(in_fm, out_fm, kernel_size=4, stride=2, padding=1, norm='none')]
+            in_fm = out_fm
+            out_fm *= 2
+        
+        # The main body of both classifier and discriminator 
+        self.model = nn.Sequential(*model)
+
+        # Discriminator and classifier heads 
+        self.classif = torch.nn.Conv2d(in_fm, self.num_outputs_drug, kernel_size=3, bias=False)   
+        self.discr = torch.nn.Conv2d(in_fm, 1, kernel_size=3, stride=1, bias=False)
+        self.activation_discr = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        # Output of the shared convolution layer 
+        h = self.model(x)
+        # Output of the classification layer 
+        out_class = self.classif(h).view(h.shape[0], -1)
+        # Output of Patch GAN
+        out_patch = self.activation_discr(self.discr(h)).mean(1).view(h.size(0))
+        return out_patch, out_class
+    
+    def discriminator_pass(self, X, X_hat, y_real, loss_discr, loss_classif):
+        self.train()
+        # Get the output of both patch and classifier 
+        out_patch_fake, _ = self.forward(X_hat.detach())
+        out_patch_real, out_classif_real = self.forward(X)
+    
+        # Discriminator loss
+        label_real = torch.ones(X.shape[0]).to(self.device) 
+        label_fake = torch.zeros(X_hat.shape[0]).to(self.device)
+        loss_real = loss_discr(out_patch_real, label_real)
+        loss_fake = loss_discr(out_patch_fake, label_fake)
+
+        # Classifier loss
+        loss_class = loss_classif(out_classif_real, y_real)  # Cross entropy 
+    
+        return (loss_real + loss_fake)/2 , loss_class
+
+    def generator_pass(self, X_hat, y_fake, loss_discr, loss_classif):
+        self.eval()
+        
+        # Swapped indices are meant to fool the classifier 
+        out_patch_fake, out_classif_fake = self.forward(X_hat)
+
+        # Classifier loss 
+        loss_class = loss_classif(out_classif_fake, y_fake)
+
+        # Discriminator loss
+        labels_fake = torch.ones(X_hat.shape[0]).to(self.device)  
+        loss_fake = loss_discr(out_patch_fake, labels_fake)
+        
+        return loss_class, loss_fake
 
 
 
