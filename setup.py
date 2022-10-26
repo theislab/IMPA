1c4b43d46fc11fae09219cf7b9613574b60667be
ICB Alessandro Palma <alessandro.palma@hpc-submit01.scidom.de>
2022-10-26 17:31:21 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='IMPA',
+    version='1.0.0',
+    description='',
+    url='https://github.com/theislab/imCPA',
+    author='Alessandro Palma, Mohammad Lotfollahi',
+    packages=['IMPA/']+find_packages(),
+    include_package_data=True
+)
