cb6833dae6de1c4d1b161691b6d170b5b715bb62
allepalma <alessandro.palma@hpc-build01.scidom.de>
2024-05-29 14:47:13 +0200

    Adapt evaluation

--- IMPA/eval/eval.py
+++ IMPA/eval/eval.py
@@ -1,6 +1,6 @@
 import pickle as pkl
-
 import numpy as np
+import random
 import torch
 from os.path import join as ospj
 
@@ -8,117 +8,124 @@ import ot
 from IMPA.eval.gan_metrics.fid import *
 from tqdm import tqdm
 from IMPA.utils import swap_attributes
+import torch.nn.functional as F
 
 
-def evaluate(nets, loader, device, dest_dir, embedding_path, args, embedding_matrix, channels=[0,1,2]):
+def evaluate(nets, loader, device, args, embedding_matrix, batch_correction, n_classes, channels=[0,1,2]):
     """Evaluate the model during training.
 
     Args:
         nets (dict): Dictionary with model networks.
         loader (torch.utils.data.DataLoader): Data loading object.
         device (str): 'cuda' or 'cpu'.
-        dest_dir (str): Directory where to save embeddings.
-        embedding_path (str): Path to the destination directory.
         args (dict): Hyperparameters for training.
         embedding_matrix (torch.Tensor): Tensor with perturbation embeddings.
+        batch_correction (bool): Whether batch correction is applied.
+        n_classes (int): Number of classes.
         channels (list, optional): List of channel indices for evaluation. Defaults to [0, 1, 2].
 
     Returns:
         dict: A dictionary storing the evaluation metrics.
     """
 
-    # Accumulated distance between true and generated images
-    wd_transformations = 0
+    # Initialize accumulated metrics
+    if not batch_correction:
+        wd_transformations = 0
     fid_transformations = 0
 
-    # Lists containing the true labels of the batch
-    y_true_ds = []
-    y_fake_ds = []
+    # Lists to store the true labels and generated images
     X_real = []
-    X_swapped = []
-
-    # Store points for the style and content encoding
-    z_basal_ds = []
-    z_style_ds = []
+    X_pred = []
+    Y_trg = []
+    if batch_correction:
+        Y_org = []
 
     # Loop over single observations
     for observation in tqdm(loader):
-        # Get the data and swap the labels
-        X = observation['X'].to(device)
-        y_one_hot_org = observation['mol_one_hot'].to(device)
-        y_org = y_one_hot_org.argmax(1).long()
-        y_trg = swap_attributes(y_one_hot_org, y_org, device).long().argmax(1)  # random swap
-
+        if batch_correction:
+            x_real = observation['X'].to(device)
+            y_org = observation['mol'].long().to(device)
+            y_org_one_hot = F.one_hot(y_org, num_classes=n_classes)
+            y_trg = swap_attributes(y_org_one_hot, y_org, device)
+            Y_org.append(y_org.to('cpu'))
+        else:
+            x_real_ctrl, x_real_trt = observation['X']
+            x_real_ctrl, x_real_trt = x_real_ctrl.to(device), x_real_trt.to(device)
+            y_trg = observation['mol'].long().to(device)
+    
         # Store perturbation labels
-        y_true_ds.append(y_org.to('cpu'))
-        y_fake_ds.append(y_trg.to('cpu'))
+        Y_trg.append(y_trg.to('cpu'))
 
         # Draw random vector for style conditioning
         if args.stochastic:
-            z = torch.randn(X.shape[0], args.z_dimension).to(device)
+            if batch_correction:
+                z = torch.randn(x_real.shape[0], args.z_dimension).to(device)
+            else:
+                z = torch.randn(x_real_ctrl.shape[0], args.z_dimension).to(device)
 
         with torch.no_grad():
             # Get perturbation embedding and concatenate with the noise vector
             z_emb = embedding_matrix(y_trg)
-            if args.stochastic:
-                z_emb = torch.cat([z_emb, z], dim=1)
+            z_emb = torch.cat([z_emb, z], dim=1)
 
             # Map to style
             s = nets.mapping_network(z_emb)
 
-            # Generate
-            z_basal, X_fake = nets.generator(X, s)
-
-            # Save real and swapped images
-            X_swapped.append(X_fake.cpu())
-            X_real.append(X.cpu())
-
-            # Save the basal state and style for later visualization
-            z_basal_ds.append(z_basal.detach().to('cpu'))
-            z_style_ds.append(s.detach().to('cpu'))
+            # Generate images
+            if batch_correction:
+                _, x_fake = nets.generator(x_real, s)
+            else:
+                _, x_fake = nets.generator(x_real_ctrl, s)
+                
+            X_pred.append(x_fake.cpu())
+            X_real.append(x_real.cpu())  
 
     # Perform list concatenation on all of the results
-    y_true_ds = torch.cat(y_true_ds).to('cpu').numpy()
-    y_fake_ds = torch.cat(y_fake_ds).to('cpu').numpy()
+    if batch_correction:
+        Y_org = torch.cat(Y_org).to('cpu').numpy()
+    Y_trg = torch.cat(Y_trg).to('cpu').numpy()
+    categories = np.unique(Y_trg)
 
     # Concatenate and get the flattened versions of the images
-    X_swapped = torch.cat(X_swapped, dim=0)
+    X_pred = torch.cat(X_pred, dim=0)
     X_real = torch.cat(X_real, dim=0)
-    categories = np.unique(y_true_ds)
-
+    
     if len(categories) > 100:
         categories = np.random.choice(categories, 100)
 
     # Update the metrics scores (FID, WD)
     for cat in tqdm(categories):
         # Compute FID/WD for a class at a time
-        X_real_cat = X_real[y_true_ds == cat]
-        X_swapped_cat = X_swapped[y_fake_ds == cat]
+        X_real_cat = X_real[Y_org == cat]
+        X_pred_cat = X_pred[Y_trg == cat]
 
-        # Wasserstein distance
-        wd = ot.emd2(torch.tensor([]), torch.tensor([]),
-                     ot.dist(X_real_cat.view(len(X_real_cat), -1),
-                             X_swapped_cat.view(len(X_swapped_cat), -1), 'euclidean'), 1)
-        wd_transformations += wd
+        if not batch_correction:
+            # Wasserstein distance
+            wd = ot.emd2(torch.tensor([]), torch.tensor([]),
+                        ot.dist(X_real_cat.view(len(X_real_cat), -1),
+                                X_pred_cat.view(len(X_pred_cat), -1), 'euclidean'), 1)
+            wd_transformations += wd
 
+        else:
+            num_samples_to_keep_real =  200
+            indices_to_keep_real = random.sample(range(len(X_real_cat)), num_samples_to_keep_real)
+            num_samples_to_keep_generated = 200
+            indices_to_keep_generated = random.sample(range(len(X_pred_cat)), num_samples_to_keep_generated)
+
+            X_real_cat = X_real_cat[indices_to_keep_real]
+            X_pred_cat = X_pred_cat[indices_to_keep_generated]
+            
         # FID
         X_real_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_real_cat.to(device)))
-        X_swapped_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_swapped_cat.to(device)))
-        fid = cal_fid(X_real_dataset, X_swapped_dataset, 2048, True, custom_channels=channels)
+        X_pred_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_pred_cat.to(device)))
+        fid = cal_fid(X_real_dataset, X_pred_dataset, 2048, True, custom_channels=channels)
         fid_transformations += fid
 
-    # Concatenate style and content vectors
-    z_basal_ds = torch.cat(z_basal_ds, dim=0).detach().cpu().numpy()
-    z_style_ds = torch.cat(z_style_ds, dim=0).detach().cpu().numpy()
-
     # Save metrics
-    dict_metrics = {'wd_transformations': wd_transformations / len(categories),
-                    'fid_transformations': fid_transformations / len(categories)}
-
-    # Dump latent embeddings
-    emb_path = ospj(dest_dir, embedding_path, 'embeddings.pkl')
-    print(f"Save embeddings at {emb_path}")
-    with open(emb_path, 'wb') as file:
-        pkl.dump([z_basal_ds, z_style_ds, y_fake_ds], file)
+    if batch_correction:
+        dict_metrics = {'fid_transformations': fid_transformations / len(categories)}
+    else:        
+        dict_metrics = {'wd_transformations': wd_transformations / len(categories),
+                        'fid_transformations': fid_transformations / len(categories)}
 
     return dict_metrics
